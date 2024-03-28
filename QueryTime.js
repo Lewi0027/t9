ebe808ad3672649979446532f38434ee8048d8f2
Henry Lewis <lewi0027@harrisburg.cs.colostate.edu>
2024-03-27 21:29:43 -0600

    lewi0027 cleaned up QueryTime

--- QueryTime.js
+++ QueryTime.js
@@ -1,10 +1,22 @@
 function getCurrentDateTime() {
     var currentDate = new Date();
 
+    var minutes = addZero(currentDate.getMinutes());
+    var seconds = addZero(currentDate.getSeconds());
+    
     var formattedDate = currentDate.getMonth() + 1 + '/' + currentDate.getDate() + '/' + currentDate.getFullYear();
-    var formattedTime = currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds();
+    var hours = currentDate.getHours()
+
+    var formattedTime = hours + ':' + minutes + ':' + seconds;
 
     return formattedDate + ' at ' + formattedTime;
+}
+
+function addZero(number) {
+    if (number < 10) {
+        number = '0' + number;
+    }
+    return number;
 }
 
 function updateAccessDate() {
