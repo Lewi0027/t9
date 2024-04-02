6ceb9ba76bf292255a1b59e24728045216051f93
R0ssP <rosspavey2002@gmail.com>
2024-04-02 13:29:06 -0600

    prevent form submission if either one is invalid

--- validate.js
+++ validate.js
@@ -7,22 +7,27 @@ function checkForm(event) {
 
     let value1 = inputForm.input1.value;
     let value2 = inputForm.input2.value;
+    let invalidInput1 = "Invalid input. Input should be a number between 1 and 26.";
+    let invalidInput2 = "Invalid input. Input should be a number between 1 and 10.";
+    let isValid = true;
 
     if (isNaN(value1) || value1 < 1 || value1 > 26) {
-        let invalidInput1 = "Invalid input. Input should be a number between 1 and 26.";
         document.getElementById("errorMessage1").textContent = invalidInput1;
         inputForm.input1.style.backgroundColor = "Red";
-        event.preventDefault();
+        isValid = false;
     }else {
-        document.getElementById("errorMessage1").textContent = invalidInput1;
+        document.getElementById("errorMessage1").textContent = "";
     }
 
     if (isNaN(value2) || value2 < 1 || value2 > 10) {
-        let invalidInput2 = "Invalid input. Input should be a number between 1 and 10.";
         document.getElementById("errorMessage2").textContent = invalidInput2;
         inputForm.input2.style.backgroundColor = "Red";
-        event.preventDefault();
+        isValid = false;
     }else {
-        document.getElementById("errorMessage2").textContent = invalidInput2;
+        document.getElementById("errorMessage2").textContent = "";
+    }
+
+    if (!isValid) {
+        event.preventDefault();
     }
 }
