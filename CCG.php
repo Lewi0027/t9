fc3c5dc41e24cbe65ad80812225cf2c98af0d7f0
Henry Lewis <lewi0027@harrisburg.cs.colostate.edu>
2024-04-07 00:49:57 -0600

    Henry Lewis workingo on CCG page

--- CCG.php
+++ CCG.php
@@ -1,4 +1,26 @@
 <!DOCTYPE html>
+<?php
+    $input1 = isset($_POST['input1']) ? $_POST['input1'] : '';
+    $input2 = isset($_POST['input2']) ? $_POST['input2'] : '';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        
+        for ($i=0; $i<=$input2; $i++) {
+            for($j=0; $j<=$input2; $j++) {
+                if (array_key_exists("$i$j", $_POST)) {
+                    $buttonValue[] = [$i, $j];
+                    goto printButtonValue;
+                }
+            }
+        }
+
+        printButtonValue:
+        if (!empty($buttonValue)) {    
+            echo '<p>Row:' . $buttonValue[0][0] . '</p><br>';
+            echo '<p>Column: ' . $buttonValue[0][1] . '</p>';
+        }
+    }
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -15,23 +37,23 @@
     <div id="colorswath">
         <h1>This is where we generate colors</h1>
             <form id="inputForm" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
-                <label for="input1">Choose a number between 1-26:</label>
+                <label for="input1">Choose a number between 1-10:</label>
                 <p id="errorMessage1" style="color: red;"></p>
-                <input type="text" id="input1" name="input1" placeholder="1-26">
+                <input type="text" id="input1" name="input1" placeholder="1-10" value="<?php echo $input1; ?>">
                 <br><br>
-                <label for="input2">Choose a number between 1-10:</label>
+                <label for="input2">Choose a number between 1-26:</label>
                 <p id="errorMessage2" style="color: red;"></p>
-                <input type="text" id="input2" name="input2" placeholder="1-10">
+                <input type="text" id="input2" name="input2" placeholder="1-26" value="<?php echo $input2; ?>">
                 <br>
-                <input type="submit">
+                <input type="submit" id="submitButton">
             </form>
         <?php
             $input1 = isset($_POST['input1']) ? $_POST['input1'] : '0';
             $input2 = isset($_POST['input2']) ? $_POST['input2'] : '0';
 
-            echo "<p>$input1 and $input2</p>"; 
+            echo "<p>Colors: $input1 <br> Rows/Columns: $input2</p><br>";
+            echo "<p id=\"clickedButton\"></p>";
         ?>
-
         <table id="table1" style="width: 80%; margin:10px auto">
             <colgroup>
                 <col style="width: 20%;">
@@ -42,7 +64,7 @@
                     echo "<tr>
                             <td>
                                 <select class = 'colorDropdown' name = 'dropdown_$i' id='dropdown_$i'>
-                                    <option value='null'>---</option>
+                                    <option value='0'>---</option>
                                     <option value='1'>Red</option>
                                     <option value='2'>Orange</option>
                                     <option value='3'>Yellow</option>
@@ -65,14 +87,48 @@
         <table id="table2" style="width: 80%; margin:10px auto">
             <?php
                 for($row = 0; $row < $input2; $row++){
-                    echo "<tr>";
+                    echo "<tr id=\"squareTable\">";
                     for($col = 0; $col < $input2; $col++){
-                        echo "<td>-</td>";
+                        $asciiValue = 65 + $col;
+                        $letterValue = chr($asciiValue);
+                        echo "<td id=\"table2td\">
+                                <form method=\"post\">
+                                    <input type=\"submit\" data-col=\"$col\" data-row=\"$row\" class= \"button\" id = \"table2button\" value=\"$letterValue$row\">
+                            </td>";
                     }
                     echo "</tr>";
                 }
             ?>
         </table>
+        <button id="ntButton">Print Preview</button>
     </div>
+    <script>
+        document.getElementById('ntButton').addEventListener('click', function() {
+            var input1 = document.getElementById('input1').value;
+            var input2 = document.getElementById('input2').value;
+            
+            var colorSelected = JSON.parse(localStorage.getItem('colorSelected')) || [];
+            var encodedColorSelected = encodeURIComponent(JSON.stringify(colorSelected));
+
+            var printPreviewUrl = 'printview.php?input1=' + input1 + '&input2=' + input2 + '&colorSelected=' + encodedColorSelected;
+
+            window.open(printPreviewUrl, '_blank');
+        });
+
+        let buttons = document.querySelectorAll('.button');
+        buttons.forEach(function(button) {
+            button.addEventListener('click', function(event) {
+                event.preventDefault();
+
+                let row = this.dataset.row;
+                let col = parseInt(this.dataset.col);
+
+                col += 65;
+                colToLetter = String.fromCharCode(col);
+
+                document.getElementById('clickedButton').innerText = "Clicked Button: " + colToLetter + row ;
+            });
+        });
+    </script>
 </body>
 </html>
--- validate.js
+++ validate.js
@@ -1,6 +1,13 @@
 let inputForm = document.querySelector("#inputForm");
 inputForm.addEventListener("submit", checkForm);
 
+let dropdowns = document.querySelectorAll(".colorDropdown");
+
+let colorSelected = [];
+let lengthValue = parseInt(inputForm.input1.value);
+colorSelected.length = lengthValue + 1;
+colorSelected.fill(0);
+
 function checkForm(event) {
     inputForm.input1.style.backgroundColor = "#3CBC8D";
     inputForm.input2.style.backgroundColor = "#3CBC8D";
@@ -32,20 +39,18 @@ function checkForm(event) {
     }
 }
 
-let dropdowns = document.querySelectorAll(".colorDropdown");
-
-let colorSelected = [];
-
 dropdowns.forEach(function(dropdown, index) {
     dropdown.addEventListener('change', function(event) {
         let selectedColor = event.target.value;
 
-        colorSelected[index] = parseInt(selectedColor);
+        colorSelected[parseInt(selectedColor)] = 1;
+        
+        localStorage.setItem('colorSelected', JSON.stringify(colorSelected));
 
         dropdowns.forEach(function(dropdownCheck) {
             let options = dropdownCheck.options;
             for (let i = 0; i < options.length; i++) {
-                options[i].disabled = colorSelected.includes(i);
+                options[i].disabled = colorSelected[i] === 1;
             }
         });
     });
